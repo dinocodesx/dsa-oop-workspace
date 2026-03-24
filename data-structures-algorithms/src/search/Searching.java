996cc1c7497352427475c5856e268d855fd761bc
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2026-03-24 16:38:03 +0530

    updated

--- data-structures-algorithms/src/search/Searching.java
+++ data-structures-algorithms/src/search/Searching.java
@@ -1,13 +1,38 @@
 package search;
 
-import java.util.Arrays;
-
 public class Searching {
     public static void main(String[] args) {
         System.out.println("Linear Search");
-        Linear.linear_search(new int[]{1, 2, 3, 4, 5}, 3);
+        linear_search(new int[]{1, 2, 3, 4, 5}, 3);
 
         System.out.println("Binary Search");
-        Binary.binary_search(new int[]{1, 2, 3, 4, 5}, 3);
+        binary_search(new int[]{1, 2, 3, 4, 5}, 3);
+    }
+
+    static void linear_search(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                System.out.println("Found at index " + (i+1));
+                return;
+            }
+        }
+    }
+
+    static void binary_search(int[] arr, int key ) {
+        int low = 0;
+        int high = arr.length - 1;
+        int mid = (low + high) / 2;
+
+        while (low <= high) {
+            if (arr[mid] == key) {
+                System.out.println("Found at index " + (mid+1));
+                return;
+            } else if (arr[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+            mid = (low + high) / 2;
+        }
     }
 }
--- data-structures-algorithms/src/sorting/Sorting.java
+++ data-structures-algorithms/src/sorting/Sorting.java
@@ -3,9 +3,52 @@ package sorting;
 public class Sorting {
     public static void main(String[] args) {
         System.out.println("Bubble Sort");
-        Bubble.bubble_sort(new int[]{5, 4, 3, 2, 1});
+        bubble_sort(new int[]{5, 4, 3, 2, 1});
 
+        System.out.println();
         System.out.println("Selection Sort");
-        Selection.selection_sort(new int[]{5, 4, 3, 2, 1});
+        selection_sort(new int[]{5, 4, 3, 2, 1});
+    }
+
+    static void selection_sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+
+    static void bubble_sort(int[] arr) {
+        boolean swapped;
+
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
     }
 }
